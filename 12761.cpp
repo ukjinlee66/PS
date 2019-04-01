f9cf51bbb5a2b350ee8b9be14715e1e9e8d269f7
ukjinlee66 <43813141+ukjinlee66@users.noreply.github.com>
2019-04-01 13:06:50 +0900

    BOJ-12761

---
+++ 12761.cpp
@@ -0,0 +1,57 @@
+﻿#include<cstdio>
+#include<queue>
+using namespace std;
+const int MAXN = 100001;
+int n, m, a,b,cnt,x;
+bool visit[MAXN];
+queue<pair<int,int>> Q;
+int main()
+{
+	scanf("%d%d%d%d", &a, &b, &n, &m);
+	Q.push(make_pair(n,0));
+	visit[n] = true;
+	while (!Q.empty())
+	{
+		int now = Q.front().first;
+		int count = Q.front().second;
+		Q.pop();
+		if (now == m)
+		{
+			printf("%d\n", count);
+			return 0;
+		}
+		if (now + 1 < MAXN && !visit[now + 1]) {
+			Q.push(make_pair(now + 1,count+1));
+			visit[now + 1] = true;
+		}
+		if (now - 1 >= 0 && !visit[now - 1]) {
+			Q.push(make_pair(now - 1,count+1));
+			visit[now - 1] = true;
+		}
+		if (now - a >= 0 && !visit[now - a]) {
+			Q.push(make_pair(now - a, count + 1));
+			visit[now - a] = true;
+		}
+		if (now + a < MAXN && !visit[now + a]) {
+			Q.push(make_pair(now + a, count + 1));
+			visit[now + a] = true;
+		}
+		if (now - b >= 0 && !visit[now - b]) {
+			Q.push(make_pair(now - b, count + 1));
+			visit[now - b] = true;
+		}
+		if (now + b < MAXN && !visit[now + b]) {
+			Q.push(make_pair(now + b, count + 1));
+			visit[now + b] = true;
+		}
+		if (now * a < MAXN && !visit[now * a]) {
+			Q.push(make_pair(now * a, count + 1));
+			visit[now*a] = true;
+		}
+		if (now * b < MAXN && !visit[now * b]) {
+			Q.push(make_pair(now * b,count+1));
+			visit[now*b] = true;
+		}
+	}
+	return 0;
+}
