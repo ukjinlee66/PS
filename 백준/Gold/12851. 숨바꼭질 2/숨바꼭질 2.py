9f8b903d42c2548b3e361a858e69d33f3f02fb02
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2023-02-01 09:20:19 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 4780 ms, Memory: 74464 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,33 @@
+import sys
+import queue
+input = sys.stdin.readline
+INF = int(1e9)
+MAXN = 100000
+N, K = map(int, input().split())
+visited = [False] * (MAXN + 1)
+q = queue.Queue()
+q.put((N,0))
+ret = INF
+retCount = 0
+time = 0
+while not q.empty():
+    current, time = q.get()
+    if current < 0 or current > MAXN:
+        continue
+    visited[current] = True
+    if current == K:
+        if ret > time:
+            ret = time
+            retCount = 1
+        elif ret == time:
+            retCount += 1
+        continue
+    time += 1
+    if (current - 1) >= 0 and not visited[current - 1]:
+        q.put(((current - 1), time))
+    if current < K and (current + 1) <= MAXN and not visited[current + 1]:
+        q.put(((current + 1), time))
+    if current < K and (current * 2) <= MAXN and not visited[current * 2]:
+        q.put(((current * 2), time))
+print(ret)
+print(retCount)
