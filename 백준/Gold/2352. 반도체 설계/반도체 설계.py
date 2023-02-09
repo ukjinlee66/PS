4b17cbb7f3a4d67c18c01cdc30076fa2f94724c8
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2023-02-09 16:56:49 +0900

    [Gold II] Title: 반도체 설계, Time: 96 ms, Memory: 36036 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+N = int(input().rstrip())
+ar = list(map(int,input().split()))
+lis = [INF] * N
+
+def binarysearch(left, right, val):
+    global lis
+
+    while (left < right):
+        mid = (right + left) // 2
+        if lis[mid] < val:
+            left = mid + 1
+        else:
+            right = mid
+    return right
+
+lis[0] = ar[0]
+idx = 1
+idx2 = 0
+while idx < N:
+    if lis[idx2] < ar[idx]:
+        lis[idx2+1] = ar[idx]
+        idx2+=1
+    else:
+        id = binarysearch(0,idx2,ar[idx])
+        lis[id] = ar[idx]
+    idx+=1
+cnt = 0
+print(idx2+1)
