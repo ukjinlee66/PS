9d456b1c0db1f676d1094f357cba439287a9175e
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2023-02-10 09:27:26 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 1252 ms, Memory: 157852 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -0,0 +1,24 @@
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+INF = int(1e9)
+N = int(input().rstrip())
+ar = list(map(int,input().split()))
+lis = []
+lis.append(ar[0])
+dp = [0] * (N+1)
+for i in range(1,N):
+    if lis[-1] < ar[i]:
+        lis.append(ar[i])
+        dp[i] = len(lis) - 1
+    else:
+        dp[i] = bisect_left(lis, ar[i])
+        lis[dp[i]] = ar[i]
+print(len(lis))
+maxlen = len(lis) - 1
+ans = []
+for i in range(N-1,-1,-1):
+    if dp[i] == maxlen:
+        ans.append(ar[i])
+        maxlen-=1
+print(*(reversed(ans)))
