e6b1513c7b699ced11aaa12e7120101fe9010a5d
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2023-02-10 14:04:21 +0900

    [Silver III] Title: N과 M (5), Time: 228 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, M = list(map(int,input().split()))
+ar = [ 0 for _ in range(N)]
+arr = sorted(list(map(int,input().split())))
+visit = [False for _ in range(10000 + 1)]
+
+def DFS(cnt):
+    global N, M, arr
+    if cnt == M:
+        for i in range(M):
+            print(ar[i],end=' ')
+        print()
+        return
+
+    for i in arr:
+        if not visit[i]:
+            visit[i]= True
+            ar[cnt] = i
+            DFS(cnt + 1)
+            visit[i]=False
+DFS(0)
