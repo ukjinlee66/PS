3e70f669fe18697daa456f63888539e7934d05bd
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2024-10-30 23:20:31 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 16344 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int M;
+    static int arr[];
+    static int result[];
+
+    public static void main(String[] args) throws IOException {
+        // long N = Long.parseLong(br.readLine());
+        // int N = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = arr[0];
+        M = arr[1];
+        result = new int[M];
+        boolean visited[] = new boolean[N];
+
+        permutation(0, visited, 0);
+
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void permutation(int depth, boolean[] visited, int start) throws IOException {
+        if (depth == M) {
+            for(int i=0;i<M;i++) {
+                bw.write(result[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i=start;i<N;i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = i+1;
+                permutation(depth+1, visited, i+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void permutation(int[] result, int depth, int N, int M) throws IOException {
+        if (depth == M) {
+            for(int i=0;i<M;i++) {
+                bw.write(result[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i=0;i<N;i++) {
+            result[depth] = i+1;
+            permutation(result, depth+1, N, M);
+        }
+    }
+
+    public static void combination(int cnt, int start) throws IOException {
+        if (cnt == M) {
+            for(int i=0;i<M;i++) {
+                bw.write(result[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i=start; i<N;i++) {
+            result[cnt] = i+1;
+            combination(cnt+1, start + 1);
+        }
+    }
+}
