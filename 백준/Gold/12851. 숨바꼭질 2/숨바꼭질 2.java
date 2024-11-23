5bb70da4be329143c37ef16b726e9b7b7c9a9177
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2024-11-23 19:06:48 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 268 ms, Memory: 90680 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int ret;
+    static int N, M;
+    static int In[];
+    static boolean visited[];
+    static Queue<Jump> q = new LinkedList<>();
+    public static void main(String[] args)    throws IOException {
+        In = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = In[0];
+        M = In[1];
+        visited = new boolean[100001];
+        ret = Integer.MAX_VALUE;
+        int cnt = 0;
+        q.offer(new Jump(In[0], 0));
+        while (!q.isEmpty()) {
+            Jump j = q.poll();
+            visited[j.x] = true;
+            if (j.x == M) {
+                if (ret > j.cnt) {
+                    ret = j.cnt;
+                    cnt = 1;
+                } else if (ret == j.cnt) {
+                    cnt++;
+                }
+                continue;
+            } else if (j.x < 0 || j.x > 100000) {
+                continue;
+            }
+            if (j.x < M) {
+                if (!visited[j.x+1])
+                    q.offer(new Jump(j.x+1, j.cnt+1));
+                if (j.x*2 <= 100000 && !visited[j.x*2])
+                    q.offer(new Jump(j.x*2, j.cnt+1));
+                if (j.x-1 >= 0 && !visited[j.x-1])
+                    q.offer(new Jump(j.x-1, j.cnt+1));
+            } else {
+                if (j.x-1 >= 0 && !visited[j.x-1])
+                    q.offer(new Jump(j.x-1, j.cnt+1));
+            }
+        }
+        bw.write(ret + "\n" + cnt + "\n");
+        bw.flush();
+        bw.close();
+    }
+    public static class Jump {
+        int x;
+        int cnt;
+        public Jump(int x, int cnt) {
+            this.x = x;
+            this.cnt = cnt;
+        }
+    }
+}
