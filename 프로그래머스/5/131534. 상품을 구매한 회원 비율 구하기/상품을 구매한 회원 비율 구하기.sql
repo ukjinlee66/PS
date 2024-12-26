ec819e8c2809ad233625c5abc06b1fe11651933e
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2024-12-26 22:52:42 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -0,0 +1,11 @@
+-- USER_INFO 테이블과 ONLINE_SALE 테이블에서 2021년에 가입한 전체 회원들 중 상품을 구매한 회원수와 상품을 구매한 회원의 비율(=2021년에 가입한 회원 중 상품을 구매한 회원수 / 2021년에 가입한 전체 회원 수)을 년, 월 별로 출력하는 SQL문을 작성해주세요. 상품을 구매한 회원의 비율은 소수점 두번째자리에서 반올림하고, 전체 결과는 년을 기준으로 오름차순 정렬해주시고 년이 같다면 월을 기준으로 오름차순 정렬해주세요.
+
+
+SELECT DATE_FORMAT(O.SALES_DATE,'%Y') YEAR,
+DATE_FORMAT(O.SALES_DATE,'%m') MONTH,
+COUNT(distinct(O.USER_ID)) PURCHASED_USERS,
+ROUND(COUNT(distinct(O.USER_ID)) / (SELECT COUNT(*) FROM USER_INFO U1 WHERE U1.JOINED BETWEEN '2021-01-01' AND '2021-12-31'), 1) PUCHASED_RATIO
+FROM ONLINE_SALE O
+JOIN (SELECT U2.USER_ID FROM USER_INFO U2 WHERE U2.JOINED BETWEEN '2021-01-01' AND '2021-12-31') P ON P.USER_ID = O.USER_ID
+GROUP BY DATE_FORMAT(O.SALES_DATE,'%Y'), DATE_FORMAT(O.SALES_DATE,'%m')
+ORDER BY DATE_FORMAT(O.SALES_DATE,'%Y'), DATE_FORMAT(O.SALES_DATE,'%m')
