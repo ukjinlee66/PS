09e8e6038f2d6122e030905f1a9555cdbb6384f1
Wookjin Lee <43813141+ukjinlee66@users.noreply.github.com>
2025-03-03 19:58:44 +0900

    [Silver III] Title: 주유소, Time: 452 ms, Memory: 41716 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,29 +5,26 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
-    static long dp[];
-    static int dist[];
-    static int cost[];
+    static long dist[];
+    static long cost[];
+    static long curMinCost;
+    static long ans;
         public static void main(String[] args)    throws IOException {
             
             N = Integer.parseInt(br.readLine());
             
-            dist = new int[N];
-            dp = new long[N];
-            cost = new int[N];
+            dist = new long[N];
+            ans = 0;
+            cost = new long[N];
 
-            Arrays.fill(dp, Integer.MAX_VALUE);
-            dist = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            cost = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            dp[0] = dist[0]*cost[0]; // init value
-            for(int i=1;i<N;i++) {
-                if (i != N-1)
-                    dp[i] = Math.min(Math.min(dp[i-1] + cost[i]*dist[i], dp[i-1] + cost[i-1]*dist[i]), dp[i]);
-                else
-                    dp[i] = dp[i-1];
+            dist = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+            cost = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+            curMinCost = cost[0];
+            for(int i=0;i<N-1;i++) {
+                curMinCost = Math.min(curMinCost, cost[i]);
+                ans+= curMinCost * dist[i];
             }
-            
-            bw.write(dp[N-1] + "\n");
+            bw.write(ans + "\n");
             bw.flush();
             bw.close();
         }
